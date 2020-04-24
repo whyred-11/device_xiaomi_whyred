3e980527420072c230329864ad177d3cbf1399a1
Roshan Pius <rpius@google.com>
2020-04-24 14:33:28 -0700

    whyred: Add WhyredWifiOverlay
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Change-Id: Iec27a6d947a232a42451ef095dc73d1a52327db5

--- device.mk
+++ device.mk
@@ -564,3 +564,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
+
+# Wifi
+PRODUCT_PACKAGES += \
+    WhyredWifiOverlay
+
+# Inherit proprietary files
+$(call inherit-product-if-exists, vendor/xiaomi/whyred/whyred-vendor.mk)
