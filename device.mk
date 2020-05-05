8ca966318dd636a7c4d6904f3deb4d9d03db9750
Pranav Vashi <neobuddy89@gmail.com>
2020-05-06 03:45:37 +0530

    sdm660-common: Add missing 64-bit interface required for IMS
    Also include only required bitness of the interfaces
    
    Change-Id: I8ef9171cd5d53cfb5ddfaae49b56696940ae6442

--- device.mk
+++ device.mk
@@ -172,13 +172,15 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.device@3.4 \
     android.frameworks.displayservice@1.0_32 \
+    android.hardware.camera.device@3.4:64 \
+    android.hardware.camera.device@3.5:64 \
+    android.hardware.camera.provider@2.4:64 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.5:64 \
     libxml2 \
-    android.hardware.camera.provider@2.5 \
-    vendor.qti.hardware.camera.device@1.0
+    vendor.qti.hardware.camera.device@1.0:64
 
 # CNE
 PRODUCT_PACKAGES += \
