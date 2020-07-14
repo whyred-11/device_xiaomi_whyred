a41f9197b345e8e2f62113d65784288fa14ce04b
Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
2020-07-14 10:24:01 +0700

    whyred: Add Moto Dolby
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>
    Signed-off-by: Mohammad Iqbal <mi460334@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -272,3 +272,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.audio.soundtrigger.phrase.adsp.level=40 \
     ro.vendor.audio.soundtrigger.user.adsp.level=60 \
     ro.vendor.audio.soundtrigger.training.level=50
+
+# Dolby
+vendor.audio.dolby.ds2.enabled=true
+
