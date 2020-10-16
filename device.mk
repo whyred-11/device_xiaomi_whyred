77d4d3cc096ef673eeefd00f2a75576cedc99e9f
SakilMondal <mondalsakil@gmail.com>
2020-10-16 17:26:50 +0530

    whyred: biometrics: Uprev to v2.2
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>

--- device.mk
+++ device.mk
@@ -230,7 +230,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint feature
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_whyred
+    android.hardware.biometrics.fingerprint@2.2-service.xiaomi_whyred
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
