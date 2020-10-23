b09aa401879a4d9c80e6caccee20e3e95b46b2ec
root2raj <ruturaj1999@gmail.com>
2020-10-23 04:19:44 +0000

    whyred: Add DT2W support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -124,6 +124,9 @@ USE_OPENGL_RENDERER := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
+# DT2W
+TARGET_TAP_TO_WAKE_NODE := "/proc/tp_gesture"
+
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 
