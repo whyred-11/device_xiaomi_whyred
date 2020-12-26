62de9db3d5232928e81fa8d19a537c1fbda4e45e
iqbal <mi460334@gmail.com>
2020-12-26 22:06:39 +0000

    Fix

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -93,7 +93,7 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 # Force Enforce
-nclude $(CLEAR_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE := init.enforcing.rc
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
