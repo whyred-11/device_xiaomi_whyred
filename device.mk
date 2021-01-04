3c6f369646fe26094d7624ce8cd6d883b11d9596
iqbal <mi460334@gmail.com>
2021-01-04 08:17:23 +0000

    Replace audio@2.0-service with audio.service

--- device.mk
+++ device.mk
@@ -102,7 +102,7 @@ PRODUCT_BOOT_JARS += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
-    android.hardware.audio@2.0-service \
+    android.hardware.audio.service \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.1-impl \
